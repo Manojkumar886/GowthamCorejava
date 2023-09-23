e9d6e655f4c4caa220532f6fbb1dc0979b95fd78
Manojkumar886 <maddymano434@gmail.com>
2023-09-23 13:29:57 +0530

    Basic Datatype is over

---
+++ src/main/java/FundamentalJava/App.java
@@ -0,0 +1,15 @@
+package FundamentalJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("-----------Global Warming123[]{};';@#------");
+        System.out.println("bdehdwzmekdkhxwbnmnsmnmxnmnmxnnnnnnnn");
+        //System.out.println( "Welcome to Java Class-\tGowtham\nHello World \n  w3school" );
+    }
+}
---
+++ src/main/java/FundamentalJava/DemoDatatypes.java
@@ -0,0 +1,17 @@
+package FundamentalJava;
+
+public class DemoDatatypes
+{
+    public static void main(String[] args)
+    {
+        System.out.println("----------Aadhar Card Details--------");
+        long AadharcardNo=9078563423452313l;
+        String AadharcardName="Manojkumar M";
+        char AadharcardType='T';
+        double Aadharcardsize=7.8;
+        System.out.println("my aadhar card no is :"+AadharcardNo);
+        System.out.println("my aadharcard full name:"+AadharcardName);
+        System.out.println("----my aadhar card details---\n"+
+                "my aadhar card name is "+AadharcardName+"\n my Aadharcard size is :"+Aadharcardsize+"\n"+AadharcardType+" this is  my addhar card type ");
+    }
+}
